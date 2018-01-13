f957577723a64c048071d39255f239fd62ae8cf2
TAN KAZUKi <buppa.mods@gmail.com>
2018-01-13 14:08:59 +0900

    Edit routes.rb
    ルーティングの一部を変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
   resource :user, only: [:edit, :update]
+  resource :groups, only: [:new, :create, :edit, :update] do
+  	resource :messages, only: [:index, :create]
+  end	
 end
