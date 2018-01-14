93ee6e7a4b47db985bf9f1eaccf15cd8dc9f7591
TAN KAZUKi <buppa.mods@gmail.com>
2018-01-14 16:28:51 +0900

    Edit route.rb
    resourcesに変更
    resource→resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'messages#index'
-  resource :user, only: [:edit, :update]
-  resource :groups, only: [:new, :create, :edit, :update] do
-  	resource :messages, only: [:index, :create]
+  root 'groups#index'
+  resources :users, only: [:edit, :update]
+  resources :groups, only: [:index, :new, :create, :edit, :update] do
+  	resources :messages, only: [:index, :create]
   end	
 end
