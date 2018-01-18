343cfe11d3a1d884d7ecc3ca45c911755708643e
TAN KAZUKi <buppa.mods@gmail.com>
2018-01-18 17:19:32 +0900

    Edit messages_controller
    mergeからgroupの値を削除

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -18,7 +18,7 @@ class MessagesController < ApplicationController
 
     private
     def message_params
-      params.require(:message).permit(:body, :image).merge(user_id: current_user.id, group_id: params[:group])  
+      params.require(:message).permit(:body, :image).merge(user_id: current_user.id)  
    end
 
     def set_group
