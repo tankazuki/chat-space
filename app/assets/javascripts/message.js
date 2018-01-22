73e9b87c46a2836317c908c892012df932c0fa2a
TAN KAZUKi <buppa.mods@gmail.com>
2018-01-22 16:51:36 +0900

    Edit Message.js
    発火以降の設定及び出力を定義

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,23 @@
 $(function(){
+　function buildHTML(message){
+    var post_image = "";
+    if (message.image) {
+      post_image = `<img src="${ message.image }">`;
+    }
+    var html = ` <div class = "chat-main__body__message-name">
+                  ${message.name}
+                 </div>
+                 <div class = "chat-main__body__message-date">
+                  ${message.date}
+                 </div>
+                 <div class = "chat-main__body__message-body">
+                  <p>${message.body}</p>
+                  ${ post_image }
+                 </div>
+               </div>`
+               console.log(html);
+    return html;
+  }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     console.log(this)
@@ -11,6 +30,17 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
-    })  
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.chat-main__body').append(html);
+     $('.chat-main__body').animate({scrollTop: $('.chat-main__body')[0].scrollHeight}, "fast");
+      $('.input-form').val('');
+      $('.send--btn').removeAttr("disabled");
+    })
+　　　.fail(function(){
+    　alert('error');
+     $('.send--btn').removeAttr("disabled");
+    })
   })
 })
