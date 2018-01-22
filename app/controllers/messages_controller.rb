75e99a74a20e6ed1a0afe8128138692ad528bf15
TAN KAZUKi <buppa.mods@gmail.com>
2018-01-22 16:52:05 +0900

    Edit respond_to
    respond_toを定義

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,14 +8,14 @@ class MessagesController < ApplicationController
   
   def create
     @message = @group.messages.new(message_params)
+    respond_to do |format|
     if @message.save
-       respond_to do |format|
         format.html {redirect_to group_messages_path(@group), notice: "メッセージの送信に成功しました"  }
         format.json
-    　 end 
     else 
       flash.now[:alert] = "メッセージの送信に失敗しました"
       render :index
+    end
     end   
   end
 
